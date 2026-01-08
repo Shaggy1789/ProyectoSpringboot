20bcdcbdc24b1c9a073952786c5570308660d8ad
Shaggy1789 <chavezrios2005@gmail.com>
2026-01-07 20:38:28 -0600

    Primer commit

---
+++ src/main/java/com/master/springboot/Controller/HelloController.java
@@ -0,0 +1,27 @@
+package com.master.springboot.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/holamundo")
+    public String holamundo(Model model){
+        model.addAttribute("mensaje", "Hola Mundo desde Springboot");
+        return "hola";
+    }
+
+//    @GetMapping("/holamundo")
+//    public Map<String,String> foo(){
+//        Map<String,String> json = new HashMap<>();
+//        json.put("Message","Hola mundo desde springboot");
+//        return json;
+//    }
+
+}
